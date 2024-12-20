635193cab813a3f8b50b8216ca492bee61527581
Project-Vigil-1602 <project.vigil.1602@gmail.com>
2024-12-20 16:30:51 +0530

    Updates

---
+++ precache-manifest.c4e795d6102980efcb996fcdc1f5c340.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306a10f6925659c0cc9e1a5fc69055e7",
+    "url": "/website_updater/index.html"
+  },
+  {
+    "revision": "7f0487c1a7b6929fea35",
+    "url": "/website_updater/static/js/2.e49af9e6.chunk.js"
+  },
+  {
+    "revision": "4d249f42d389787ff562b8c4e1786a99",
+    "url": "/website_updater/static/js/2.e49af9e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "741efe30f3523dbb533f",
+    "url": "/website_updater/static/js/main.a129ad14.chunk.js"
+  },
+  {
+    "revision": "8b3f8ba943c498f82e4e",
+    "url": "/website_updater/static/js/runtime-main.2409a466.js"
+  }
+]);
